99a6edd13855913d96fbca06a39c8029ed21864e
vadim010975 <vadim010975@mail.ru>
2023-09-18 20:59:00 +0300

    init

---
+++ src/Team.js
@@ -0,0 +1,21 @@
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error ('Дублирование объекта');
+    } else {
+      this.members.add(character);
+    }
+  }
+
+  addAll(...characters) {
+    characters.forEach(el => this.members.add(el));
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
